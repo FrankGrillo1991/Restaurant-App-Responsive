78f7ff790cdf98b910bf19a075d258530ce08083
FrankGrillo1991 <fsg12491@aol.com>
2025-04-13 15:46:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,45 @@ const reviewDots = document.querySelectorAll("#review-carousel .absolute button"
 let currentReview = 0;
 let intervalId;
 
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("hidden");
+}
+
+hamburgerBtn.addEventListener("click", toggleMobileMenu);
+closeMenuBtn.addEventListener("click", toggleMobileMenu);
+mobileMenuLinks.forEach(link => {
+    link.addEventListener("click", toggleMobileMenu)
+});
+
+document.addEventListener("click", (event) => {
+    if (!mobileMenu.classList.contains("hidden") && !mobileMenu.contains(event.target) && event.target !== hamburgerBtn) {
+        toggleMobileMenu();
+    }
+});
+
+function updateActiveNavLink() {
+    let currentSectionId = '';
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (window.scrollY >= sectionTop - 100 && window.scrollY < sectionTop + sectionHeight - 100) {
+            currentSectionId = section.getAttribute("id")
+        }
+    });
+}
+
+window.addEventListener("scroll", updateActiveNavLink);
+
+navLinks.forEach(link => {
+    link.addEventListener("click", (event) => {
+        event.preventDefault();
+        const targetId = link.getAttribute("href").slice(1);
+        const targetSection = document.getElementById(targetId);
+        if (targetSection) {
+            window.scrollTo({
+                tap: targetSection.offsetTop - 80,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
